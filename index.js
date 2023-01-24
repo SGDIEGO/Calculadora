8a8d3ccf8bc22a6e6c50e7fc0f1c3913664f34d8
SGDIEGO <diegoalonsosg@gmail.com>
2023-01-24 16:56:41 -0500

    Add restart button to calculator.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let arregloValores =
-    [[1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 9],
-     ['=', 0, null, '/'],
-    ]
+let arregloNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, '', 0, ''];
+
+let arregloOpciones = ['+', '-', '*', '/', '='];
 
 $(document).ready(() => {
+    arregloNumeros.forEach(e => {
+        $(".teclas-numeros").append(`<button>${e}</button>`);
+    });
 
+    arregloOpciones.forEach(e => {
+        $(".teclas-opciones").append(`<button>${e}</button>`);
+    });
 });
